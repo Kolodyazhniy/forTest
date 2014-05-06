4a745b04198a98d6155c5ef6e7139a6158de7668
alexwan <alexwan1122@gmail.com>
2014-05-06 17:16:09 +0300

    1

---
+++ shl2_for/src/ForTest.java
@@ -0,0 +1,63 @@
+
+public class ForTest {
+	public static void main(String args[]) {
+		int x;
+		
+	for(x = 0; x<10; x = x+1)
+		System.out.println("Значение x: " + x) ;
+	
+
+	
+//	boolean b;
+//	b = false;
+//	System.out.println("b равна " + b);
+//	b = true;
+//	System.out.println("b равна " + b);
+//	// значение типа boolean может управлять оператором if
+//	if(b) System.out.println("Это выполняется.");
+//	b = false;
+//	if(!b) System.out.println("Это не выполняется.");
+//	// результат сравнения - значение типа boolean
+//	System.out.println("10 > 9 равно " + (10 > 9));
+	
+	
+	long l1=1999999999;
+	long l2=29999999;
+	
+	long sum = l1+l2;
+	
+	System.out.println(+sum);
+	
+	
+	double fl=   1.4;
+	
+	double dl =6.346666666666666;
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
+}
