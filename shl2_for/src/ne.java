f904bc02ae36087ce0cb16820b52c0a522fef2cb
alexwan <alexwan1122@gmail.com>
2014-05-06 17:30:51 +0300

    7667

---
+++ shl2_for/src/ne.java
@@ -0,0 +1,19 @@
+
+public class ne {
+	
+	int n0 = 0, n1 = 1, n2=3, n3;
+	System.out.println(n0 + " " + n1 + " " + " ");
+if (n0<0) throw new IllegalArgumentException("x must>0");           
+	for (int i = 0; i < 25; i++) {
+
+		n3 = n2 + n1 + n0;
+		System.out.println(n3 + " ");
+		n0 = n1;
+		n1 = n2;
+		n2 = n3;
+	}
+	
+
+}
+
+}
